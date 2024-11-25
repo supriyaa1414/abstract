fb724cc79efcbeeef4285143d79f0bc096441b3e
supriyaa1414 <152264823+supriyaa1414@users.noreply.github.com>
2024-11-25 14:54:19 +0530

    Update Mainjava.java

--- Mainjava.java
+++ Mainjava.java
@@ -1,45 +1,58 @@
-package mycodes;
+package basics;
 
-public class Mainjava{
-	 
-    // Method to add two integers
-    public int add(int a, int b) {
-        return a + b;
+abstract class Shape {
+    String colour;
+    int area;
+
+    String setShape(String newColour) {
+        this.colour = newColour;
+        return this.colour;
     }
- 
-    // Method to add three integers
-    public int add(int a, int b, int c) {
-        return a + b + c;
+
+    abstract void calcArea();
+}
+
+class Rectangle extends Shape {
+    int length;
+    int breadth;
+    int height;
+
+    Rectangle(int length, int breadth, int height) {
+        this.length = length;
+        this.breadth = breadth;
+        this.height = height;
     }
- 
-    // Method to add two floating-point numbers
-    public float add(float a, float b) {
-        return a + b;
-    }
- 
-    // Method to add an array of integers
-    public int add(int[] numbers) {
-        int sum = 0;
-        for (int num : numbers) {
-            sum += num;
-        }
-        return sum;
-    }
- 
-    public static void main(String[] args) {
-    	Mainjava addition = new Mainjava();
-        // Add two integers
-        System.out.println("Sum of two integers: " + addition.add(5, 10));
- 
-        // Add three integers
-        System.out.println("Sum of three integers: " + addition.add(5, 10, 15));
- 
-        // Add two floating-point numbers
-        System.out.println("Sum of two floats: " + addition.add(5.5f, 10.5f));
- 
-        // Add an array of integers
-        int[] numbers = {1, 2, 3, 4, 5};
-        System.out.println("Sum of array: " + addition.add(numbers));
+
+    
+    void calcArea() {
+        this.area = length * breadth * height;
+        System.out.println("Rectangle area: " + area + " (Color: " + colour + ")");
     }
 }
 
+class Circle extends Shape {
+    int radius;
+
+    Circle(int radius) {
+        this.radius = radius;
+    }
+
+    
+    void calcArea() {
+        this.area = (int) (Math.PI * radius * radius);
+        System.out.println("Circle area: " + area + " (Color: " + colour + ")");
+    }
+}
+
+public class Transportation {
+    public static void main(String[] args) {
+       
+        Rectangle rectangle = new Rectangle(10, 3, 14);
+        rectangle.setShape("Pink");
+        rectangle.calcArea();
+
+        Circle circle = new Circle(8);
+        circle.setShape("Red");
+        circle.calcArea(); 
+    }
+}
